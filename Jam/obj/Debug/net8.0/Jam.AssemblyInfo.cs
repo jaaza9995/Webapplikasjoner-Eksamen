96f3296bbe9bc0bf00c2eaafd4719c98598846a9
Jara <143405392+jaaza9995@users.noreply.github.com>
2025-10-22 11:59:07 +0200

    e

--- Jam/obj/Debug/net8.0/Jam.AssemblyInfo.cs
+++ Jam/obj/Debug/net8.0/Jam.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jam")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d5fee292f3e1c845210e1afeb213e92070fbd64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0130497cef582acfb65a076cc6c6c1f80fc2115a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Jam")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Jam")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
